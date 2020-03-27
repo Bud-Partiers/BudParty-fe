b16161b884c123892948ee26885b592d2737dc3e
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-03-27 16:55:14 -0500

    edited html, added image

--- budparty/src/App.js
+++ budparty/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import greenBoquet from './images/green-bouquet.jpg';
 import roseVase from './images/rose-vase.jpg';
+import groupPic from './images/group.jpg'
 
 function App() {
   return (
@@ -10,14 +11,14 @@ function App() {
 
       </header>
       <div className="container">
-        <h1 className='title'>Wine and Roses</h1>
+        <h1 className='title'>Bud Party</h1>
         <div className='cta-images'>
           <img src={roseVase} />
           <img src={greenBoquet} />
         </div>
         <div className="info">
           <h2 className='contact-title'>BOOK A PRIVATE CLASS</h2>
-          <a className='msg' href='https://m.me/flowersandclasses?fbclid=IwAR3IgNUGCt8RWVpBAjfbKPDpaM_xgi4r1uMzCJa0_EtgQ5gnPNLlJtieh-E'>Send a Message</a>
+          {/* <a className='msg' href='https://m.me/flowersandclasses?fbclid=IwAR3IgNUGCt8RWVpBAjfbKPDpaM_xgi4r1uMzCJa0_EtgQ5gnPNLlJtieh-E'>Send a Message</a> */}
           <div className='contacts'>
 
             <div className='item'>
@@ -30,6 +31,7 @@ function App() {
             </div>
 
           </div>
+          <img className='group-pic' src={groupPic} />
         </div>
 
 
